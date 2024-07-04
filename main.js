145cb232dc6efd9005cdc04cd1be20f9ecddedc6
AnmolAqsa <165940283+AnmolAqsa@users.noreply.github.com>
2024-07-04 12:45:36 -0700

    Add files via upload
    Q21, 45 Projects

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+Q21. They think of something you could store in a TypeScript Object.
+Write a program that creates Objects containing these items.
+*/
+var object = {
+    Name: "Laptop",
+    Generation: 3,
+    Speed: "Fast",
+    Color: "Gray",
+    Price: 24000,
+    Instoke: true,
+};
+console.log(object);
---
+++ main.ts
@@ -0,0 +1,15 @@
+/*
+Q21. They think of something you could store in a TypeScript Object. 
+Write a program that creates Objects containing these items.
+*/
+
+let object={
+        Name: "Laptop",
+        Generation: 3,
+        Speed: "Fast",
+        Color: "Gray",
+        Price: 24000,
+        Instoke: true,
+
+    }
+    console.log(object);
